fa54888b7ad8c91638f221c6d1a7063f967ab53e
Shota Okugawa <okusho721@gmail.com>
2022-04-03 10:32:23 +0900

    text response to check userID

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -38,14 +38,14 @@ class WebhookController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each do |event|
-      response = "#{event.userID} #{event.displayName}"
+      #response = "event.userID" "event.displayName"
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: response
+            text: event.userID
           }
           client.reply_message(event['replyToken'], message)
         end
